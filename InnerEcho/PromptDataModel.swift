f9c6e6bb418ae453a39eebf0e99ef1e1f7bc1ba7
Kal <kal.aalrajhi@gmail.com>
2023-07-21 14:49:05 -0400

    Create basic data model for prompts

---
+++ InnerEcho/PromptDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  PromptDataModel.swift
+//  InnerEcho
+//
+//  Created by Dr Cpt Blackbeard on 7/21/23.
+//
+
+import Foundation
+
+struct PromptModel: Identifiable, Codable {
+    let id: Int
+    let title: String
+}
