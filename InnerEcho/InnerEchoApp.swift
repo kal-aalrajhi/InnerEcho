5039d892452bad3b0a7f91592025d8f8d500ef57
Kal <kal.aalrajhi@gmail.com>
2023-07-21 13:16:19 -0400

    Initial Commit

---
+++ InnerEcho/InnerEchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerEchoApp.swift
+//  InnerEcho
+//
+//  Created by Dr Cpt Blackbeard on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InnerEchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
