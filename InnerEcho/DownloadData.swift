9753c45311af686d197f10f60041411ad7fcdec6
Kal <kal.aalrajhi@gmail.com>
2023-07-24 10:50:42 -0400

    Create URL checker helper function

--- InnerEcho/DownloadData.swift
+++ InnerEcho/DownloadData.swift
@@ -10,32 +10,17 @@ import Foundation
 import Combine
 
 class DownloadData: ObservableObject {
-    @Published var prompts: [Prompt] = []
-    @Published var currentURL: URL
-    
+    @Published var prompts: [Prompt] = []    
     // Used in conjunction with publishers to control when a subscription should be cancelled
     // Creating an empty set of Cancellables - by default publishers return cancellable instances and immediatly cancel subs
     var cancellables = Set<AnyCancellable>()
     
     init() {
-        getPrompts()
+//        getPrompts()
     }
     
-    func getPrompts() {
-//        getURL()
-        
-//        URLSession.shared.dataTaskPublisher(for: url)
-//            .subscribe(on: DispatchQueue.global(qos: .background)) // sub to publisher on BG thread
-//            .receive(on: DispatchQueue.main) // recieve on the main thread
-//            .tryMap { (data, response) -> Data in
-//                guard let response = response as? HTTPURLResponse,
-//                response.statusCode >= 200 && response.statusCode < 300 else {
-//                    print("Response is not the correct type, please make sure it's a valid HTTPURLResponse type")
-//                    throw URLError(.badServerResponse)
-//                }
-//                return data
-//            }
-            .decode(type: [Prompt].self, decoder: JSONDecoder())
+    func getPrompts(from url: URL) {
+            checkURL(url).decode(type: [Prompt].self, decoder: JSONDecoder())
             .sink { completion in
                 print("COMPLETION: \(completion)") // returns finished or failure
                 
@@ -51,21 +36,24 @@ class DownloadData: ObservableObject {
             .store(in: &cancellables)
     }
     
-    func setURL(from url: URL) {
+    // Helper function
+    private func checkURL(_ url: URL) -> AnyPublisher<Data, Error> {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/albums") else {
             print("Invalid URL.")
-            
-        URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .background)) // sub to publisher on BG thread
-            .receive(on: DispatchQueue.main) // recieve on the main thread
+            fatalError("Invalid URL.") // There's no way to recover from this error, so we'll just cause the program to crash.
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background)) // subscribe to publisher on background thread
+            .receive(on: DispatchQueue.main) // receive on the main thread
             .tryMap { (data, response) -> Data in
                 guard let response = response as? HTTPURLResponse,
-                response.statusCode >= 200 && response.statusCode < 300 else {
+                      response.statusCode >= 200 && response.statusCode < 300 else {
                     print("Response is not the correct type, please make sure it's a valid HTTPURLResponse type")
                     throw URLError(.badServerResponse)
                 }
                 return data
             }
-        }
+            .eraseToAnyPublisher()
     }
 }
